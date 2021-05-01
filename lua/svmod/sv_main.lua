6a9cf0c5f00d4f8c6132fe62fbad457dab92c98e
Tom <tom.marquaille@epita.fr>
2021-05-02 00:08:57 +0200

    Fixed error on conflict detected

--- lua/svmod/sv_main.lua
+++ lua/svmod/sv_main.lua
@@ -7,7 +7,7 @@
 function SVMOD:Enable()
 	-- Do not start SVMod if conflict detected
 	if #self:GetConflictList() > 0 then
-		self:PrintConsole(SVMOD.LOG.Error, language.GetPhrase("svmod.cannot_start_conflict"))
+		self:PrintConsole(SVMOD.LOG.Error, "Unable to start SVMod: conflict detected.")
 		self.CFG.IsEnabled = false
 		return false
 	end
@@ -79,20 +79,28 @@ hook.Add("PlayerConnect", "SV_EnableAddon", function()
 	end)
 end)
 
--- concommand.Add("temp", function(ply)
--- 	local ent = ply:GetEyeTrace().Entity
+ concommand.Add("temp", function(ply)
+ 	local ent = ply:GetEyeTrace().Entity
 
--- 	local size = 0
+ 	--local size = 0
+	 --
+ 	--ent:SetSpringLength(0, 499.95)
+ 	-- -- ent:SetSpringLength(1, 499.95)
+ 	-- -- ent:SetSpringLength(2, 499.95)
+ 	-- -- ent:SetSpringLength(3, 499.95)
+	 --
+ 	--timer.Simple(2, function()
+ 	--	ent:SetSpringLength(0, 500.12)
+ 	--	ent:SetSpringLength(1, 500.12)
+ 	--	ent:SetSpringLength(2, 500.12)
+ 	--	ent:SetSpringLength(3, 500.12)
+ 	--end)
 
--- 	ent:SetSpringLength(0, 499.95)
--- 	-- ent:SetSpringLength(1, 499.95)
--- 	-- ent:SetSpringLength(2, 499.95)
--- 	-- ent:SetSpringLength(3, 499.95)
+	 for i = 1, ent:GetWheelCount() do
+		 local tyre = ent:GetWheel(i)
 
--- 	timer.Simple(2, function()
--- 		ent:SetSpringLength(0, 500.12)
--- 		ent:SetSpringLength(1, 500.12)
--- 		ent:SetSpringLength(2, 500.12)
--- 		ent:SetSpringLength(3, 500.12)
--- 	end)
--- end)
+		 if tyre:IsValid() then
+			 ent:SetSpringLength(i - 1, 500.08 + 0.1)
+		 end
+	 end
+ end)
