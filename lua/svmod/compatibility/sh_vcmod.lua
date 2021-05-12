a19190c32e4d1741e9e40860fe618271a90f5bc0
Tom <tom.marquaille@epita.fr>
2021-05-12 20:31:30 +0200

    Added VCMod API partial compatibility

---
+++ lua/svmod/compatibility/sh_vcmod.lua
@@ -0,0 +1,41 @@
+hook.Add("SV_Enabled", function()
+	if VCMOD then
+		return
+	end
+
+	function SVMOD.Metatable:VC_fuelGet(percent)
+		if percent then
+			return self:SV_GetPercentFuel()
+		else
+			return self:SV_GetFuel()
+		end
+	end
+	function SVMOD.Metatable:VC_GetFuel(percent)
+		return self:VC_fuelGet(percent)
+	end
+
+	function SVMOD.Metatable:VC_getHealth(percent)
+		if CLIENT or percent then
+			return self:SV_GetPercentHealth()
+		else
+			return self:SV_GetHealth()
+		end
+	end
+	function SVMOD.Metatable:VC_GetHealth(percent)
+		return self:VC_getHealth(percent)
+	end
+
+	function SVMOD.Metatable:VC_getHealthMax()
+		return self:SV_GetMaxHealth()
+	end
+	function SVMOD.Metatable:VC_GetMaxHealth()
+		return self:VC_getHealthMax()
+	end
+
+	function SVMOD.Metatable:VC_getDamagedParts()
+		return {}
+	end
+	function SVMOD.Metatable:VC_GetDamagedParts()
+		return self:VC_getDamagedParts()
+	end
+end)
---
+++ lua/svmod/compatibility/sv_vcmod.lua
@@ -0,0 +1,40 @@
+hook.Add("SV_Enabled", function()
+	if VCMOD then
+		return
+	end
+
+	function SVMOD.Metatable:VC_fuelAdd(amount)
+		self:SV_SetFuel(self:SV_GetFuel() + amount)
+	end
+	function SVMOD.Metatable:VC_Fuel_Add(amount)
+		self:VC_fuelAdd(amount)
+	end
+
+	function SVMOD.Metatable:VC_fuelConsume(amount)
+		self:SV_SetFuel(self:SV_GetFuel() - amount)
+	end
+	function SVMOD.Metatable:VC_Fuel_Consume(amount)
+		self:VC_fuelConsume(amount)
+	end
+
+	function SVMOD.Metatable:VC_damageHealth(amount)
+		return self:SV_SetHealth(self:SV_GetHealth() - amount)
+	end
+	function SVMOD.Metatable:VC_DamageHealth(amount)
+		return self:VC_damageHealth(amount)
+	end
+
+	function SVMOD.Metatable:VC_repairFull_Admin()
+		return self:SV_SetHealth(self:SV_GetMaxHealth())
+	end
+	function SVMOD.Metatable:VC_RepairFull_Admin()
+		return self:VC_repairFull_Admin()
+	end
+
+	function SVMOD.Metatable:VC_setDamagedParts(table)
+
+	end
+	function SVMOD.Metatable:VC_SetDamagedParts(table)
+		self:VC_setDamagedParts(table)
+	end
+end)
