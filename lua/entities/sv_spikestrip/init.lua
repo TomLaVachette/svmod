6ef930e7abe4d51ed393319bd0bc7a900b4e5539
Romain Lejeune <romlejeune@outlook.fr>
2021-12-22 21:29:54 +0100

    Implement spikestrip

---
+++ lua/entities/sv_spikestrip/init.lua
@@ -0,0 +1,31 @@
+AddCSLuaFile("cl_init.lua")
+AddCSLuaFile("shared.lua")
+
+include("shared.lua")
+
+
+function ENT:Initialize()
+	self:SetModel( "models/spikestrip/spikestrip.mdl" )
+	self:PhysicsInit( SOLID_VPHYSICS )
+	self:SetMoveType( MOVETYPE_NONE )
+	self:SetSolid( SOLID_VPHYSICS )
+	self:SetUseType( SIMPLE_USE )
+	self:SetTrigger( true )
+end
+
+function ENT:Use( activator, caller )
+end
+
+function ENT:Think()
+end
+
+function ENT:Touch(ent)
+	print(math.random(40,80))
+	if ent:GetClass() == "prop_vehicle_jeep" and SVMOD:IsVehicle(ent) then
+		for wheel = 0, ent:GetWheelCount() do
+			if ent:GetWheelContactPoint(wheel):Distance( self:GetTouchTrace().HitPos ) < 50 then
+				ent:SV_DealDamageToWheel(wheel + 1, math.random(40,80))
+			end
+		end
+	end
+end
---
+++ lua/entities/sv_spikestrip/shared.lua
@@ -0,0 +1,6 @@
+ENT.Type = "anim"
+ENT.Base = "base_gmodentity"
+ENT.PrintName = "Herse"
+ENT.Author = "Romain Lejeune"
+ENT.Category = "SVMod"
+ENT.Spawnable = true
