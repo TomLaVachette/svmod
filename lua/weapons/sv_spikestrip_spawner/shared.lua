8a64ca5576e597d85bd205695d7f2b3c60fdd134
wow <srp@wowow.cloud>
2021-12-23 19:20:31 +0100

    Fixed SpikeStrip

--- lua/weapons/sv_spikestrip_spawner/shared.lua
+++ lua/weapons/sv_spikestrip_spawner/shared.lua
@@ -3,48 +3,25 @@ SWEP.Instructions   = "Left click to deploy spike strips, Right click to collect
 SWEP.ViewModel      = "models/weapons/c_crowbar.mdl"
 SWEP.WorldModel 	= "models/weapons/w_package.mdl"
 SWEP.HoldType 		= "normal"
-SWEP.Spawnable		= true
 SWEP.UseHands 		= true
-SWEP.Base 			= "wep_ck_base"
+SWEP.Spawnable		= true
+SWEP.AdminOnly		= true
 
-SWEP.Primary.Recoil		= 5
-SWEP.Primary.Damage		= 0
-SWEP.Primary.NumShots		= 0
-SWEP.Primary.Cone			= 0.075
-SWEP.Primary.Delay 		= 1.5
+SWEP.Primary = {
+	ClipSize = -1,
+	DefaultClip = -1,
+	Automatic = false,
+	Ammo = "none"
+}
 
-SWEP.Primary.ClipSize		= -1
-SWEP.Primary.DefaultClip	= -1
-SWEP.Primary.Automatic		= true
-SWEP.Primary.Ammo			= ""
+SWEP.Secondary = {
+	ClipSize = -1,
+	DefaultClip = -1,
+	Automatic = false,
+	Ammo = "none"
+}
 
-SWEP.Secondary.ClipSize		= -1
-SWEP.Secondary.DefaultClip	= -1
-SWEP.Secondary.Automatic	= false
-SWEP.Secondary.Ammo		= "none"
-
-SWEP.ShellEffect			= "none"
-SWEP.ShellDelay			= 0
-
-SWEP.Sequence			= 0
-
-SWEP.ShowViewModel = true
+SWEP.Used 			= false
+SWEP.Sequence		= 0
+SWEP.ShowViewModel 	= true
 SWEP.ShowWorldModel = false
-
-
-function SWEP:PrimaryAttack()
-	local tr = {}
-		tr.start = self.Owner:GetShootPos()
-		tr.endpos = self.Owner:GetShootPos() + 100 * self.Owner:GetAimVector()
-		tr.filter = {self.Owner}
-		local trace = util.TraceLine(tr)
-		if trace.Hit and trace.HitNormal.z > 0.95 then
-			local spikestrip = ents.Create("sv_spikestrip")
-			spikestrip:SetPos(trace.HitPos)
-			spikestrip:SetAngles( Angle(0,90 + self:GetAngles().y,0) )
-			spikestrip:Spawn()
-			self:Remove()
-			self:SetNextPrimaryFire(CurTime() + 1)
-		end
-
-end
