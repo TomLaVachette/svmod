3964ab7518d6eb48103a37674b0f48a395fd5ee9
Romain Lejeune <romlejeune@outlook.fr>
2021-12-23 17:56:33 +0100

    implement spikestrip weapon

--- lua/entities/sv_spikestrip/init.lua
+++ lua/entities/sv_spikestrip/init.lua
@@ -5,7 +5,7 @@ include("shared.lua")
 
 
 function ENT:Initialize()
-	self:SetModel( "models/spikestrip/spikestrip.mdl" )
+	self:SetModel( "models/novacars/spikestrip/spikestrip.mdl" )
 	self:PhysicsInit( SOLID_VPHYSICS )
 	self:SetMoveType( MOVETYPE_NONE )
 	self:SetSolid( SOLID_VPHYSICS )
@@ -20,12 +20,19 @@ function ENT:Think()
 end
 
 function ENT:Touch(ent)
-	print(math.random(40,80))
 	if ent:GetClass() == "prop_vehicle_jeep" and SVMOD:IsVehicle(ent) then
 		for wheel = 0, ent:GetWheelCount() do
 			if ent:GetWheelContactPoint(wheel):Distance( self:GetTouchTrace().HitPos ) < 50 then
 				ent:SV_DealDamageToWheel(wheel + 1, math.random(40,80))
 			end
 		end
+	end
+end
+
+function ENT:Use(ent)
+
+	if IsValid(ent) then
+		self:Remove()
+		ent:Give("sv_spikestrip_spawner")
 	end
 end
---
+++ lua/weapons/sv_spikestrip_spawner/cl_init.lua
@@ -0,0 +1,5 @@
+include('shared.lua')
+SWEP.PrintName			= 'Spike Strip'
+SWEP.Slot				= 2
+SWEP.SlotPos			= 2
+SWEP.DrawAmmo			= false
